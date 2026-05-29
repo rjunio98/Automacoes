44b9322fdbbc7f587ef2700ee304ff7e5fbc60f5
Robledo Junior <robledorjunior1@gmail.com>
2026-05-29 00:29:02 -0300

    percorrendo pelos pdfs

--- projeto1.py
+++ projeto1.py
@@ -4,3 +4,10 @@ import os
 merger = PyPDF2.PdfFileMerger()
 
 lista_arquivos = os.listdir("arquivos")
+lista_arquivos.sort()
+
+for arquivo in lista_arquivos:
+    if ".pdf" in arquivo:
+        merger.append(f"arquivos/{arquivo}")
+
+merger.write("PDF Final.pdf")
